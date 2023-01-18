5f5a60965cd4fbbc81ad8de4529e6cf86dc0d3ad
MaryamLemsyeh <lemsyeh17maryam@gmail.com>
2023-01-18 11:52:31 +0100

    last commit

--- Billing-service/Billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
+++ Billing-service/Billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
@@ -2,6 +2,7 @@ package org.sid.billingservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.sid.billingservice.model.Product;
@@ -9,7 +10,7 @@ import org.sid.billingservice.model.Product;
 import javax.persistence.*;
 
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class ProductItem {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
